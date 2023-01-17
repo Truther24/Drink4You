fd7552a5f9aec92628749a9c2ca111dd2513f746
alex <alexbarbu240@gmail.com>
2023-01-17 13:26:07 +0200

    aaaa

---
+++ El-Proyecte-Grande/Services/SimpleCocktailService.cs
@@ -0,0 +1,22 @@
+﻿
+using el_proyecte_grande.Models;
+using el_proyecte_grande.Repositories;
+
+namespace el_proyecte_grande.Services
+{
+    public class SimpleCocktailService
+    {
+        private readonly CocktailRepository _cocktailRepository;
+        public SimpleCocktailService(CocktailRepository cocktailRepository)
+        {
+            this._cocktailRepository = cocktailRepository;
+        }
+
+        public async Task<List<SimpleCocktail>> GetALlCocktails()
+        {
+            return await _cocktailRepository.GetCocktailsFromApi();
+        }
+
+
+    }
+}
