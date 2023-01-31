84df54d63e51bc409120c62f4351791882d28891
Mishanschi <lela.2005@mail.ru>
2023-01-31 11:53:16 +0200

    database added

--- El-Proyecte-Grande/Program.cs
+++ El-Proyecte-Grande/Program.cs
@@ -1,5 +1,8 @@
+using El_Proyecte_Grande.Models;
 using El_Proyecte_Grande.Repositories;
 using El_Proyecte_Grande.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DrinkContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                });
+
 
 builder.Services.AddScoped<DrinkCategoryService>();
 builder.Services.AddScoped<DrinkCategoryRepository>();
---
+++ el-proyecte-grande/Models/DrinkContext.cs
@@ -0,0 +1,20 @@
+﻿using El_Proyecte_Grande.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace El_Proyecte_Grande.Models
+{
+    public class DrinkContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+
+
+
+        public DrinkContext(DbContextOptions<DrinkContext> options) : base(options)
+        {
+
+        }
+    }
+}
